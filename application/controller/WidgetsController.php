05ed3ddc5366993a7f429c2d43d833f2de23966f
Kevin Choppin <kevinchoppin@gmail.com>
2012-09-08 22:29:53 +0100

    Navigation with active state

--- application/controller/WidgetsController.php
+++ application/controller/WidgetsController.php
@@ -21,15 +21,6 @@ class WidgetsController extends Controller {
     function navigation(){
         
         $nav_items = $this->model->getNav();
-        
-
-         foreach($nav_items AS $nav_item_key => $nav_item_value){
- 
-             $nav_items[$nav_item_key]['active'] = false;
-            
-         }
-
-        
         $this->set('nav_items', $nav_items);
         
     }
--- application/model/Widget.php
+++ application/model/Widget.php
@@ -12,9 +12,23 @@ class Widget extends Model {
     
     function getNav() {
         
-        $stmt = $this->db_conn->prepare('SELECT * FROM navigation ORDER BY nav_order');
+        $stmt = $this->db_conn->prepare('SELECT title, page_id, url FROM navigation ORDER BY nav_order');
         $stmt->execute();
-        return $stmt->fetchAll();
+        
+        $i = 0;
+        $nav_items = array();
+        
+        while($row = $stmt->fetch()) {
+            
+            $nav_items[$i]['title'] = $row['title'];
+            $nav_items[$i]['url'] = (!empty($row['page_id'])) ? '/pages/view/'.$row['page_id'] : $row['url'] ;
+            $nav_items[$i]['active'] = ($_SERVER['REQUEST_URI'] == $nav_items[$i]['url']) ? true : false;
+            
+        $i++;
+            
+        }
+        
+        return $nav_items;
         
     }
     
